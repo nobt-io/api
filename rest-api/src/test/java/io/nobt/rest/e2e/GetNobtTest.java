b8971aade84620991968fc3c7f5c745e521905ee
Thomas Eizinger <thomas.eizinger@senacor.com>
2016-03-26 03:36:07 +0100

    add assertions to e2e test for retrieving nobt

--- rest-api/src/test/java/io/nobt/rest/e2e/GetNobtTest.java
+++ rest-api/src/test/java/io/nobt/rest/e2e/GetNobtTest.java
@@ -2,16 +2,16 @@ package io.nobt.rest.e2e;
 
 import io.nobt.rest.NobtApplication;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import spark.Spark;
 
-import static com.jayway.restassured.RestAssured.get;
 import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.JsonConfig.jsonConfig;
+import static com.jayway.restassured.config.RestAssuredConfig.newConfig;
+import static com.jayway.restassured.path.json.config.JsonPathConfig.NumberReturnType.DOUBLE;
 import static io.nobt.rest.JsonFileReader.readFromResource;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 
 public class GetNobtTest {
 
@@ -32,11 +32,19 @@ public class GetNobtTest {
 
 	@Test
 	public void testShouldReturnNobt() throws Exception {
-		get(location)
-				.then()
-				.statusCode(200)
-				.body("", is(nullValue()));
-
-		Assert.fail("Implement matching!");
+		given()
+				.config(newConfig().jsonConfig(jsonConfig().numberReturnType(DOUBLE)))
+				.get(location)
+					.then()
+					.statusCode(200)
+					.body("transactions", hasSize(2))
+					.body("transactions[0].debtee", is("Thomas"))
+					.body("transactions[0].amount", is(10.00d))
+					.body("transactions[0].debtor", is("Matthias"))
+					.body("transactions[1].debtee", is("Thomas"))
+					.body("transactions[1].amount", is(10.00d))
+					.body("transactions[1].debtor", is("Thomas B."))
+					.body("id", is(notNullValue()))
+					.body("expenses", is(notNullValue()));
 	}
 }
