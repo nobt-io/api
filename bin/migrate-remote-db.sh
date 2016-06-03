ecc2a9e8725b50d351de8c4ab031045928f46634
Thomas Eizinger <thomas@nobt.io>
2016-06-03 13:52:55 +0200

    use manifest file for migrations app to improve readability use custom command for starting app that runs infinitely to prevent cf from detecting app as crashed

--- bin/migrate-remote-db.sh
+++ bin/migrate-remote-db.sh
@@ -2,6 +2,4 @@
 
 ./gradlew :sql:dZ
 
-cf bind-service migrations-app elephant-sql
-cf push migrations-app --no-hostname --no-route --no-manifest --health-check-type none -m 128MB -b java_buildpack -p sql/build/distributions/*.zip
-# cf stop migrations-app # TODO otherwise CF keeps restarting the app because it is detected as "crash" as exits as soon as the migration is finished
+cf push -f sql/manifest.yml -p sql/build/distributions/*.zip
