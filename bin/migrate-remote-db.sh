4112436d5013bdfa6c2c02f5a69408e382a629cf
Thomas Eizinger <thomas@nobt.io>
2016-06-03 02:20:46 +0200

    fix invalid cf push command

--- bin/migrate-remote-db.sh
+++ bin/migrate-remote-db.sh
@@ -3,4 +3,4 @@
 ./gradlew :sql:dZ
 
 cf bind-service migrations-app elephant-sql
-cf push migrations-app --no-hostname --no-route --no-manifest --health-check-type -b java_buildpack -p sql/build/distributions/*.zip
+cf push migrations-app --no-hostname --no-route --no-manifest --health-check-type none -b java_buildpack -p sql/build/distributions/*.zip
