7b95e737c183cc55229f2a700a64cc74866170d8
Thomas Eizinger <thomas@nobt.io>
2016-06-19 20:06:44 +0200

    deprecate get persons handler

--- rest-api/src/main/java/io/nobt/rest/handler/GetPersonsHandler.java
+++ rest-api/src/main/java/io/nobt/rest/handler/GetPersonsHandler.java
@@ -1,7 +1,5 @@
 package io.nobt.rest.handler;
 
-import io.nobt.core.domain.Nobt;
-import io.nobt.persistence.NobtDao;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -10,18 +8,14 @@ import java.util.UUID;
 
 public class GetPersonsHandler implements Route {
 
-	private final NobtDao nobtDao;
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
 
-	public GetPersonsHandler(NobtDao nobtDao) {
-		this.nobtDao = nobtDao;
-	}
+        final UUID nobtId = UUID.fromString(request.params(":nobtId"));
 
-	@Override
-	public Object handle(Request request, Response response) throws Exception {
+        response.status(303);
+        response.header("Location", request.url() + "/" + nobtId);
 
-		final UUID nobtId = UUID.fromString(request.params(":nobtId"));
-		final Nobt nobt = nobtDao.get(nobtId);
-
-		return nobt.getParticipatingPersons();
-	}
+        return null;
+    }
 }
