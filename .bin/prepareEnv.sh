c01d2fec684f7b86879e9facc291414c82fa8d5d
Thomas Eizinger <thomas@nobt.io>
2017-01-15 21:47:55 +0100

    Use version 1.0.3 of build image.
    Version 1.0.3 comes with our custom get-env plugin for the cf cli. This plugin allows us to fetch configuration values from an app. We therefore no longer have to rely on a hardcoded application id in our CI configuration.

--- .bin/prepareEnv.sh
+++ .bin/prepareEnv.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
 
-[ -z "$1" ] && echo "App ID not given." && exit 1
 
 cf set-env \
 nobt-io \
 DATABASE_CONNECTION_STRING \
-$(cf curl /v2/apps/$1/env | jq '.system_env_json.VCAP_SERVICES.elephantsql[0].credentials.uri' | cut -d "\"" -f 2)
+$(cf get-env nobt-io ".system_env_json.VCAP_SERVICES.elephantsql[0].credentials.uri" | cut -f 1)
 
 # this script is only run on gitlab CI through .gitlab-ci.yml which means we can rely on global variables (CI_BUILD_REF)
 cf set-env \
