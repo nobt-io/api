659c30f62925100b189e268a13cfc303303c3427
Thomas Eizinger <thomas@nobt.io>
2017-01-15 21:48:29 +0100

    Make sure the space we want to target exists.

--- .bin/login.sh
+++ .bin/login.sh
@@ -4,4 +4,6 @@
 
 cf api https://api.run.pivotal.io
 cf auth $CF_BUILD_USER $CF_BUILD_USER_PASSWORD
+
+cf create-space $1 # Create space so we can assure that we can switch to it safely. Doesn't error if it already exists.
 cf target -o nobt.io -s $1
