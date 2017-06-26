b9049dfba1785356fa6e83a36feeadccbeff3df9
Thomas Eizinger <thomas@nobt.io>
2017-06-26 18:32:39 +0200

    Fix inconsistent transaction optimization

--- core/src/test/java/io/nobt/core/TransactionListOptimizerTest.java
+++ core/src/test/java/io/nobt/core/TransactionListOptimizerTest.java
@@ -1,15 +1,14 @@
 package io.nobt.core;
 
 import io.nobt.core.domain.Transaction;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static io.nobt.core.domain.Transaction.transaction;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 public class TransactionListOptimizerTest {
 
@@ -33,8 +32,8 @@ public class TransactionListOptimizerTest {
 
         List<Transaction> optimalTransactions = optimizer.getOptimalTransactions();
 
-        Assert.assertThat(optimalTransactions, hasSize(5));
-        Assert.assertThat(optimalTransactions, containsInAnyOrder(
+        assertThat(optimalTransactions, hasSize(5));
+        assertThat(optimalTransactions, containsInAnyOrder(
                 transaction("Matthias", 4, "Thomas"),
                 transaction("Jaci", 2, "Thomas B."),
                 transaction("Matthias", 10, "Thomas B."),
@@ -43,4 +42,44 @@ public class TransactionListOptimizerTest {
                 )
         );
     }
+
+    @Test
+    public void orderOfTransactionsShouldNotInfluenceResults() {
+
+        List<Transaction> transactionListA = Arrays.asList(
+                transaction("Matthias", 10, "Thomas"),
+                transaction("Thomas", 3, "David"),
+                transaction("Thomas", 10, "David"),
+                transaction("David", 13, "Thomas B."),
+                transaction("Jaci", 7, "Thomas"),
+                transaction("Thomas B.", 6, "Jaci"),
+                transaction("Jaci", 5, "Thomas B."),
+                transaction("Matthias", 4, "Jaci"),
+                transaction("Thomas", 13, "Thomas B."),
+                transaction("David", 17, "Thomas")
+        );
+
+        List<Transaction> transactionListB = Arrays.asList(
+                transaction("Matthias", 4, "Jaci"),
+                transaction("Matthias", 10, "Thomas"),
+                transaction("Thomas", 3, "David"),
+                transaction("Thomas", 13, "Thomas B."),
+                transaction("David", 13, "Thomas B."),
+                transaction("Jaci", 7, "Thomas"),
+                transaction("Thomas B.", 6, "Jaci"),
+                transaction("Jaci", 5, "Thomas B."),
+                transaction("Thomas", 10, "David"),
+                transaction("David", 17, "Thomas")
+        );
+
+        TransactionListOptimizer optimizerA = new TransactionListOptimizer(transactionListA);
+        List<Transaction> optimalTransactionsA = optimizerA.getOptimalTransactions();
+
+        TransactionListOptimizer optimizerB = new TransactionListOptimizer(transactionListB);
+        List<Transaction> optimalTransactionsB = optimizerB.getOptimalTransactions();
+
+        assertThat(optimalTransactionsA, equalTo(optimalTransactionsB));
+    }
+
+
 }
