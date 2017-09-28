aa5222fc71f0657cffae0742ac78e7156cb8e30e
Thomas Eizinger <thomas@nobt.io>
2017-09-28 22:55:56 +0200

    Refactoring copying and sorting into one statement

--- core/src/main/java/io/nobt/core/optimizer/SelfSortingMinimalAmountTransferredOptimizerStrategy.java
+++ core/src/main/java/io/nobt/core/optimizer/SelfSortingMinimalAmountTransferredOptimizerStrategy.java
@@ -2,20 +2,22 @@ package io.nobt.core.optimizer;
 
 import io.nobt.core.domain.Debt;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.toList;
 
 public class SelfSortingMinimalAmountTransferredOptimizerStrategy implements OptimizerStrategy {
 
     @Override
     public List<Debt> optimize(List<Debt> debts) {
 
-        final ArrayList<Debt> copy = new ArrayList<>(debts);
-        copy.sort(comparingInt(Debt::hashCode));
+        final List<Debt> sortedCopy = debts
+                .stream()
+                .sorted(comparingInt(Debt::hashCode))
+                .collect(toList());
 
-        return new MinimalAmountTransferredOptimizer(copy).getDebtsWithMinimalAmountTransferred();
+        return new MinimalAmountTransferredOptimizer(sortedCopy).getDebtsWithMinimalAmountTransferred();
     }
 
 }
