62ce459c9759f4525bfffb4483c598894aec474f
Thomas Eizinger <thomas@nobt.io>
2017-10-29 21:17:54 +0100

    Specify minor version of postgres image
    On AWS, we use postgres 9.6. We should also use that locally.

--- .bin/run-integration-tests-against-docker-container.sh
+++ .bin/run-integration-tests-against-docker-container.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CONTAINER_ID=$(docker run -d -e POSTGRES_PASSWORD=password -p 5432:5432 postgres:9)
+CONTAINER_ID=$(docker run -d -e POSTGRES_PASSWORD=password -p 5432:5432 postgres:9.6)
 
 if ! docker top $CONTAINER_ID &>/dev/null
 then
