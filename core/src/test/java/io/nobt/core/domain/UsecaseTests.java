c15ba257dc7ef6954af0437c3799f3d7ceead0e1
Thomas Eizinger <thomas@nobt.io>
2017-11-12 00:52:48 +0100

    Remove dead code

--- core/src/test/java/io/nobt/core/domain/UsecaseTests.java
+++ core/src/test/java/io/nobt/core/domain/UsecaseTests.java
@@ -5,13 +5,16 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static io.nobt.core.domain.debt.Debt.debt;
 import static io.nobt.core.optimizer.Optimizer.MINIMAL_AMOUNT_V2;
+import static io.nobt.test.domain.factories.AmountFactory.amount;
 import static io.nobt.test.domain.factories.ShareFactory.randomShare;
-import static io.nobt.test.domain.factories.StaticPersonFactory.thomas;
+import static io.nobt.test.domain.factories.ShareFactory.share;
+import static io.nobt.test.domain.factories.StaticPersonFactory.*;
 import static io.nobt.test.domain.provider.ExpenseBuilderProvider.anExpense;
 import static io.nobt.test.domain.provider.NobtBuilderProvider.aNobt;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
+import static io.nobt.test.domain.provider.PaymentDraftBuilderProvider.aPaymentDraft;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class UsecaseTests {
@@ -35,4 +38,37 @@ public class UsecaseTests {
         assertThat(optimizedDebts, is(empty()));
     }
 
+
+    @Test
+    public void givenASingleBill_paymentShouldSettleDebt() throws Exception {
+
+        final Nobt nobt = aNobt()
+                .withOptimizer(MINIMAL_AMOUNT_V2)
+                .withExpenses(
+                        anExpense()
+                                .withDebtee(thomas)
+                                .withShares(
+                                        share(david, 5),
+                                        share(matthias, 5)
+                                )
+                )
+                .build();
+
+        final PaymentDraft paymentDraft = aPaymentDraft()
+                .withSender(david)
+                .withRecipient(thomas)
+                .withAmount(amount(5))
+                .build();
+
+        nobt.createPaymentFrom(paymentDraft);
+
+
+        final List<Debt> optimizedDebts = nobt.getOptimizedDebts();
+
+
+        assertThat(optimizedDebts, allOf(
+                iterableWithSize(1),
+                hasItem(debt(matthias, amount(5), thomas))
+        ));
+    }
 }
