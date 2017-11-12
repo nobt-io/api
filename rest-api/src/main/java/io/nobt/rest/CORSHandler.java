b9dc5893de4ab0d0584e31e04dd8303cbaa8368b
Thomas Eizinger <thomas@nobt.io>
2017-11-12 01:18:14 +0100

    Limit CORS header to localhost and nobt.io
    Fixes #17

---
+++ rest-api/src/main/java/io/nobt/rest/CORSHandler.java
@@ -0,0 +1,27 @@
+package io.nobt.rest;
+
+import spark.Filter;
+import spark.Request;
+import spark.Response;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CORSHandler implements Filter {
+
+    private final List<String> originWhitelist = Arrays.asList(
+            "http://localhost:3000",
+            "http://nobt.io"
+    );
+
+    @Override
+    public void handle(Request request, Response response) throws Exception {
+
+        final String origin = request.headers("Origin");
+
+        if (originWhitelist.contains(origin)) {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", "*");
+        }
+    }
+}
