9b598123894cc7c1ed8fb008308a852d3c2959ef
Thomas Eizinger <thomas@nobt.io>
2017-12-10 16:43:55 +0100

    Refactor to EntityMigration

--- db-migrations/src/main/java/db/migration/v15_2/V15_2__Persist_Nobt_IDs_in_DB.java
+++ db-migrations/src/main/java/db/migration/v15_2/V15_2__Persist_Nobt_IDs_in_DB.java
@@ -1,42 +1,7 @@
 package db.migration.v15_2;
 
-import org.flywaydb.core.api.migration.MigrationChecksumProvider;
-import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.JdbcTemplate;
+import db.migration.EntityManagerMigration;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
+public class V15_2__Persist_Nobt_IDs_in_DB extends EntityManagerMigration<V15_2_NobtEntity> {
 
-public class V15_2__Persist_Nobt_IDs_in_DB implements SpringJdbcMigration, MigrationChecksumProvider {
-
-    @Override
-    public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
-
-        final List<Long> ids = jdbcTemplate.query("SELECT id from nobts", (rs, rowNum) -> rs.getLong("id"));
-
-        jdbcTemplate.batchUpdate("UPDATE nobts SET externalId = ? WHERE id = ?", new BatchPreparedStatementSetter() {
-            @Override
-            public void setValues(PreparedStatement ps, int i) throws SQLException {
-
-                final Long currentId = ids.get(i);
-
-                final String externalId = ShortURL.encode(PseudoCrypter.pseudoCryptLong(currentId));
-
-                ps.setString(1, externalId);
-                ps.setLong(2, currentId);
-            }
-
-            @Override
-            public int getBatchSize() {
-                return ids.size();
-            }
-        });
-    }
-
-    @Override
-    public Integer getChecksum() {
-        return V15_2__Persist_Nobt_IDs_in_DB.class.getName().hashCode();
-    }
 }
